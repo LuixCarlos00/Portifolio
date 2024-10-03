e43246a96cfd37492c3628264a6656759304bf2a
LuixCarlos00 <107255451+LuixCarlos00@users.noreply.github.com>
2024-10-03 15:42:48 -0300

    luiz carlos
    - Componente de envio de email criado

---
+++ src/app/enviar-email/enviar-email.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enviar-email',
+  standalone: true,
+  imports: [],
+  templateUrl: './enviar-email.component.html',
+  styleUrl: './enviar-email.component.css'
+})
+export class EnviarEmailComponent {
+
+}
