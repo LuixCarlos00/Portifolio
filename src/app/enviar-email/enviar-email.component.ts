edd2114803ab45806be9423d10fc6dcc676dfd36
LuixCarlos00 <107255451+LuixCarlos00@users.noreply.github.com>
2024-10-04 21:25:24 -0300

    Luiz carlos 04/10/2024
    - Logica implementada com sucesso, envio de email foi criado utilizado o   EmailJS.com

--- src/app/enviar-email/enviar-email.component.ts
+++ src/app/enviar-email/enviar-email.component.ts
@@ -1,26 +1,34 @@
-import { Component } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
- import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-enviar-email',
-   templateUrl: './enviar-email.component.html',
-  styleUrl: './enviar-email.component.css'
+  templateUrl: './enviar-email.component.html',
+  styleUrls: ['./enviar-email.component.css']  // Corrigido para styleUrls
 })
-export class EnviarEmailComponent {
+export class EnviarEmailComponent implements OnInit {
 
-   sendEmail(form: NgForm) {
+  ngOnInit() {
+    emailjs.init('Ohii6Rgm12xly6gxv'); // Inicialização com a chave pública
+  }
 
+  sendEmail(form: NgForm) {
+    const templateParams = {
+       from_name: form.value.toName,
+      to_email: form.value.toEmail, // Corrigido para to_email
+      message: form.value.toMessage
+    };
 
-    emailjs.send("service_yah7w5u","template_wb5vqkm",{
-      Subject: "Oferta de Trabalho",
-      name: form.value.toName,
-      email: form.value.fromName,
-      mensage: form.value.message,
+    emailjs.send('service_yah7w5u', 'template_wb5vqkm', templateParams)
+      .then((response: EmailJSResponseStatus) => {
+        console.log('SUCCESS!', response.status, response.text);
+        form.reset();
+        swal('Email enviado com sucesso!', 'Obrigado por entrar em contato!', 'success');
+      }, (error) => {
+         swal('Erro ao enviar o email!', 'Por favor, tente novamente mais tarde.', 'error');
+        console.log('FAILED...', error);
       });
-
-
-
-
   }
 }
