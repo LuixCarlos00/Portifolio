6dc421967822e9049c00a0655254f1868f308eb3
LuixCarlos00 <107255451+LuixCarlos00@users.noreply.github.com>
2024-10-22 15:56:25 -0300

    Luiz carlos 22/10/2024
    - Foi modificado e melhorado o carrocel de imgens, agora ele começa do canto esquerdo e vai para a direita  e fica em repetição

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,12 +2,9 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SnakeComponent } from './snake/snake.component';
 import { Router } from '@angular/router';
-import { PortfolioItem, Portifolio } from '../Opcoes/Portifolio';
-import { CertificadoModalComponent } from '../CertificadoModal/CertificadoModal.component';
-import { Certificados } from '../Opcoes/Certificados';
-import { ExemplosPortifolioComponent } from '../Exemplos-Portifolio/Exemplos-Portifolio.component';
-import { Esperiencia, Imagens, sobre } from '../Opcoes/Info';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+  import { ExemplosPortifolioComponent } from '../Exemplos-Portifolio/Exemplos-Portifolio.component';
+import {   Imagens } from '../Opcoes/Info';
+import { DomSanitizer  } from '@angular/platform-browser';
 import { EnviarEmailComponent } from '../enviar-email/enviar-email.component';
 import { SanitizerService } from '../service/sanitizer.service';
 
@@ -18,25 +15,16 @@ import { SanitizerService } from '../service/sanitizer.service';
   styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-  [x: string]: any;
-  portfolioItems: PortfolioItem[] = Portifolio;
-  safeHtmlDescriptions: SafeHtml[] = [];
-
-  visibleItems: PortfolioItem[] = [];
-  currentPage = 0;
-  itemsPerPage = 3; // Valor inicial para desktops
 
 
-  Esperiencia = Esperiencia;
-  IMAGENS = Imagens;
+
+   IMAGENS = Imagens;
 
   constructor(public dialog: MatDialog, public router: Router,private sanitizer: DomSanitizer,
-    private sanitizerService: SanitizerService // Injeção do serviço
+    private sanitizerService: SanitizerService
 
   ) {
-    this.safeHtmlDescriptions = this.portfolioItems.map(item =>
-      this.sanitizer.bypassSecurityTrustHtml(item.description)
-    );
+
   }
 
 
@@ -80,4 +68,4 @@ export class MenuComponent implements OnInit {
       height: '400px',
     });
   }
-} //https://www.thingiverse.com/thing:6764614
+}
